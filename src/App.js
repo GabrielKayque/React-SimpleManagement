32921313cf520a603faded5c7c821ae3e63731b7
GabrielKayque <mr.biiiel@hotmail.com>
2022-05-22 02:37:19 -0300

    import nav and home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+
+import Navbar from './components/navbar';
+import Home from './views/home';
+
 function App() {
   return (
-    <div className="alert alert-dismissible alert-warning">
-      <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
-      <h4 className="alert-heading">Warning!</h4>
-      <p className="mb-0">Best check yo self, you're not looking too good. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, <a href="//www.youtube.com" className="alert-link">vel scelerisque nisl consectetur et</a>.</p>
+    <div className='container'>
+      <Navbar />
+      <Home />
     </div>
   );
 }
