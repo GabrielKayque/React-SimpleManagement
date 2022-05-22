cd7e5a22bb2abc0136fd84caaf0fce728054f7f9
GabrielKayque <mr.biiiel@hotmail.com>
2022-05-22 02:37:04 -0300

    main component

---
+++ src/views/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+    return(
+        <div class="jumbotron">
+            <h1 class="display-3">Welcome!</h1>
+            <p class="lead">Use the navbar to access each page.</p>
+            <hr class="my-4"/>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a>
+                </p>
+        </div>
+    )
+}
+
+export default Home
