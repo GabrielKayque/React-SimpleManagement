6f97e9fa551b912217a510a90c0d775fa3770979
GabrielKayque <mr.biiiel@hotmail.com>
2022-05-22 02:15:37 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="alert alert-dismissible alert-warning">
+      <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
+      <h4 className="alert-heading">Warning!</h4>
+      <p className="mb-0">Best check yo self, you're not looking too good. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, <a href="//www.youtube.com" className="alert-link">vel scelerisque nisl consectetur et</a>.</p>
     </div>
   );
 }
