453f20fb26a7a1731a17accf7e40210d1fe7bc95
GabrielKayque <mr.biiiel@hotmail.com>
2022-05-23 19:22:49 -0300

    save system

---
+++ src/app/productService.js
@@ -0,0 +1,18 @@
+ const PRODUCTS = '_PRODUCTS';
+
+ export default class ProductService {
+    save = (product) => {
+        let products = localStorage.getItem(PRODUCTS)
+
+        if(!products) {
+            products = []
+        }else {
+            products = JSON.parse(products)
+        }
+
+        products.push(product);
+
+        localStorage.setItem(PRODUCTS, JSON.stringify(products))
+    }
+
+ }
