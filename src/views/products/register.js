38de687674518f0f8abca6d80568c66051b2dd54
GabrielKayque <mr.biiiel@hotmail.com>
2022-05-23 17:49:27 -0300

    Register class component

---
+++ src/views/products/register.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class RegisterProduct extends React.Component {
+
+    render() {
+        return (
+            <div className="card">
+                <div className="card-header">
+                     Register Product
+                </div>
+                <div className="card-body">
+
+                </div>
+            </div>
+        );
+    }
+
+}
