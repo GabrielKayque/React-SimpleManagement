9eb2c62d7109f5048b590563d9e3326c0316c00f
GabrielKayque <mr.biiiel@hotmail.com>
2022-05-30 06:15:58 -0300

    route with parameter to editing

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,20 @@
 import React from "react";
 
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Home from "./views/home";
 import RegisterProduct from "./views/products/register";
+import ProductsCatalog  from "./views/products/catalog";
 
 export default () => {
     return (
-        
-            <Router>
-                <Routes>
-                    <Route exact element={<RegisterProduct/>} path="/register-products" />
-                    <Route exact element={<Home/>} path="/" />
-                </Routes>
-            </Router>
+
+        <Routes>
+            <Route element={<RegisterProduct />} path="/register-products/:sku" />
+            <Route element={<RegisterProduct />} path="/register-products" />
+            <Route element={<ProductsCatalog />} path="/catalog" />
+            <Route exact element={<Home />} path="/" />
+        </Routes>
+
     );
 }
