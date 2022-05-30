13ec38307d1d770ad28ca3852d75bc43ae01933e
GabrielKayque <mr.biiiel@hotmail.com>
2022-05-30 06:16:48 -0300

    changed a few class to className

--- src/views/home.js
+++ src/views/home.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function Home() {
     return(
-        <div class="jumbotron">
-            <h1 class="display-3">Welcome!</h1>
-            <p class="lead">Use the navbar to access each page.</p>
-            <hr class="my-4"/>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a>
+        <div className="jumbotron">
+            <h1 className="display-3">Welcome!</h1>
+            <p className="lead">Use the navbar to access each page.</p>
+            <hr className="my-4"/>
+                <p className="lead">
+                    <a  className="btn btn-primary btn-lg" href="#" role="button">Sign Up</a>
                 </p>
         </div>
     )
