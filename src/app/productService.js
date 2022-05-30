004b65800d6fce1ab4b26eb291d7c082d31f2603
GabrielKayque <mr.biiiel@hotmail.com>
2022-05-30 02:54:06 -0300

    validate seeking errors

--- src/app/productService.js
+++ src/app/productService.js
@@ -1,7 +1,30 @@
  const PRODUCTS = '_PRODUCTS';
 
+ export function ValidationError(errors) {
+     this.errors = errors;
+ }
+
  export default class ProductService {
+
+    validate = (product) => {
+        const errors = []
+
+        // possible errors
+        if(!product.name) errors.push('NAME is mandatory!')     
+        if(!product.sku) errors.push('SKU is mandatory!')    
+        if(!product.price || product.price <= 0) errors.push('PRICE needs to be greater than 0!')
+        if(!product.supplier) errors.push('SUPPLIER is mandatory')
+
+
+        if(errors.length > 0) {
+            throw new ValidationError(errors)
+        }
+    }
+
+
     save = (product) => {
+        this.validate(product)
+
         let products = localStorage.getItem(PRODUCTS)
 
         if(!products) {
